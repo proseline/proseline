9144557cfa4358c6d3c3f04633a384ed29eafea4
Kyle E. Mitchell <kyle@kemitchell.com>
2020-11-25 22:22:44 -0800

    Check for more environment variables in production

--- check-environment.js
+++ check-environment.js
@@ -1,5 +1,6 @@
 module.exports = () => {
   const requiredEnvironmentVariables = [
+    'NODE_ENV',
     'BASE_HREF',
     'CSRF_KEY',
     'STRIPE_PLAN',
@@ -7,9 +8,24 @@ module.exports = () => {
     'STRIPE_SECRET_KEY',
     'STRIPE_WEBHOOK_SECRET'
   ]
+  const productionEnvironmentVariables = [
+    'ADMIN_EMAIL',
+    'S3_ACCESS_KEY',
+    'S3_BUCKET',
+    'S3_ENDPOINT',
+    'S3_SECRET_KEY',
+    'SMTP_HOST',
+    'SMTP_PASSWORD',
+    'SMTP_USER'
+  ]
   const missing = []
   requiredEnvironmentVariables.forEach(key => {
     if (!process.env[key]) missing.push(key)
   })
+  if (process.env.NODE_ENV === 'production') {
+    productionEnvironmentVariables.forEach(key => {
+      if (!process.env[key]) missing.push(key)
+    })
+  }
   return missing
 }
