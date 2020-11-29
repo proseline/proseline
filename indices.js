41b459ed21afdb493833f8336029a07d7f9dff63
Kyle E. Mitchell <kyle@kemitchell.com>
2020-11-29 09:22:54 -0800

    Add comment and sanity checks to indices module

--- indices.js
+++ indices.js
@@ -1,3 +1,6 @@
+// Convert numbers to and from lexically ordered keys.
+
+const assert = require('assert')
 const zeroFill = require('zero-fill')
 
 const INDEX_WIDTH = 15
@@ -7,6 +10,12 @@ exports.stringify = function (index) {
   return zeroFill(INDEX_WIDTH, MAX_INDEX - index)
 }
 
+assert(exports.stringify(0) === '999999999999999')
+assert(exports.stringify(1) === '999999999999998')
+
 exports.parse = function (string) {
   return MAX_INDEX - parseInt(string)
 }
+
+assert(exports.parse('999999999999999') === 0)
+assert(exports.parse('999999999999998') === 1)
