e1f995a7fcfc749f5ce5cb6121f5ed3bce60fdde
Kyle E. Mitchell <kyle@kemitchell.com>
2020-11-29 11:19:34 -0800

    Fix project creation test

--- test/project.test.js
+++ test/project.test.js
@@ -6,7 +6,7 @@ const subscribe = require('./subscribe')
 const verifyLogIn = require('./verify-login')
 const webdriver = require('./webdriver')
 
-tape.only('create project', test => {
+tape('create project', test => {
   const handle = 'ana'
   const password = 'test password'
   const email = 'ana@example.com'
@@ -31,12 +31,23 @@ tape.only('create project', test => {
     await project.waitForExist({ timeout: 20000 })
     await project.click()
 
-    const title = await browser.$('input[name=title]')
+    const projectTitle = 'Test Project'
+    const title = await browser.$('#projectForm input[name=title]')
     await title.waitForExist({ timeout: 20000 })
-    await title.setValue('Test Project')
+    await title.setValue(projectTitle)
 
-    const submitProject = await browser.$('button[type=submit]')
+    const submitProject = await browser.$('#projectForm button[type=submit]')
     await submitProject.click()
+
+    const h2 = await browser.$('h2=Test Project')
+    await h2.waitForExist()
+    test.pass('project page')
+
+    // Navigate back to homepage.
+    await browser.navigateTo('http://localhost:' + port)
+    const projectLink = await browser.$(`a=${projectTitle}`)
+    await projectLink.waitForExist()
+    test.pass('project link on homepage')
 
     test.end()
     done()
